916fade211a8a413dc724d81fea3e9b8e58b6643
Serge Toarca <serge@toarca.com>
2023-10-28 16:34:01 -0400

    Preloading sounds for words

--- images/psapp/client/src/modules/common/READ_WORDS/ModuleBuilder.tsx
+++ images/psapp/client/src/modules/common/READ_WORDS/ModuleBuilder.tsx
@@ -26,6 +26,34 @@ export let ModuleBuilder = ({
   return (props: void) => {
     let moduleContext = React.useContext(ModuleContext);
 
+    React.useEffect(() => {
+      let promises = [];
+      for (let variant of variants) {
+        promises.push(new Promise((resolve) => {
+          let audio = new Audio(variant.spoken);
+          audio.addEventListener('canplaythrough', resolve);
+        }));
+        for (let [_, sound] of variant.sounds) {
+          promises.push(new Promise((resolve) => {
+            let audio;
+            if (sound in LETTER_SOUNDS) {
+              audio = new Audio(
+                LETTER_SOUNDS[sound as keyof typeof LETTER_SOUNDS]
+              );
+            } else {
+              audio = new Audio(
+                BIGRAM_SOUNDS[sound as keyof typeof BIGRAM_SOUNDS]
+              );
+            }
+            audio.addEventListener('canplaythrough', resolve);
+          }));
+        }
+      }
+      Promise.all(promises).then(() => {
+        console.log('all sounds are loaded');
+      });
+    }, [variants]);
+
     let vlist = React.useMemo(() => new VariantList(variants, 5), []);
     let generateExercise = React.useCallback(() => {
       return {
