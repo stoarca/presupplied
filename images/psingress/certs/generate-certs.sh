34ecdf02bf65bf74b4547cab174fd4e3815c8a86
Serge Toarca <serge@toarca.com>
2025-02-03 20:13:43 -0500

    Fix cert management

--- images/psingress/certs/generate-certs.sh
+++ images/psingress/certs/generate-certs.sh
@@ -1,1 +1,21 @@
-openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout ./selfsigned.key -out ./selfsigned.crt -subj /CN=*.presupplied.com
+openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout presupplied-selfsigned.key -out presupplied-selfsigned.crt -config <(cat <<EOF
+[ req ]
+distinguished_name = req_distinguished_name
+x509_extensions = v3_ca
+prompt = no
+
+[ req_distinguished_name ]
+CN = presupplied-root-ca
+
+[ v3_ca ]
+subjectAltName = @alt_names
+basicConstraints = critical,CA:TRUE
+keyUsage = keyCertSign, cRLSign, digitalSignature, keyEncipherment
+subjectKeyIdentifier = hash
+
+[ alt_names ]
+DNS.1 = *.presupplied.com
+DNS.2 = applocal.presupplied.com
+EOF
+)
+
